934ae383ad31bc80358571f6425d50f6aaafd60c
Mackenzie Moore <macmoore@ucdavis.edu>
2019-05-06 23:51:13 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -49,7 +49,6 @@ class TweetsController < ApplicationController
     end
 
     patch '/tweets/:id' do
-
       @tweet = Tweet.find(params[:id])
       if logged_in? && (params[:content] != "")
         @tweet.content = params[:content]
